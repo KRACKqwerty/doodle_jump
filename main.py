6f39da2c95f31ec7aac519fab8ffcfbda1070f4b
KRACKqwerty <152689225+KRACKqwerty@users.noreply.github.com>
2023-12-09 13:37:27 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def main() ->None:
-    pass
-
+    import pygame
+    from scripts.app import App
+    pygame.init()
+    app=App()
+    app.run()
+    pygame.quit()
+    
 if __name__ == "__main__":
     main()
