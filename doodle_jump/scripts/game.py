8fbfca9c380aad235e0b2117487709f265047e4a
KRACKqwerty <152689225+KRACKqwerty@users.noreply.github.com>
2024-01-13 13:36:12 +0300

    Delete doodle_jump directory

--- doodle_jump/scripts/game.py
+++
@@ -1,6 +0,0 @@
-import pygame, os
-class Game():
-    def __init__(self):
-        self.background_image=pygame.image.load(os.path.join('assets','images','background.png'))
-    def render(self,surface:pygame.Surface):
-        surface.blit(self.background_image, (0,0))
